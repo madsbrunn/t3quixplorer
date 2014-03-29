8e56044f6b05bcdeafc37985fd6eceaf2f891f35
Stefan Froemken <froemken@gmail.com>
2014-03-29 15:43:14 +0100

    create basic directories to change ext to extbase

--- ext_tables.php
+++ ext_tables.php
@@ -1,18 +1,25 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+if (TYPO3_MODE === 'BE') {
 
-
-if (TYPO3_MODE=='BE')	{
-	
-	t3lib_extMgm::addModule('tools','txt3quixplorerM1',"",t3lib_extMgm::extPath($_EXTKEY).'mod1/');
-
-
-
-
+	/**
+	 * Registers a Backend Module
+	 */
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+		'MadsBrunn.' . $_EXTKEY,
+		'tools',	 // Make module a submodule of 'web'
+		't3quixplorer',	// Submodule key
+		'',						// Position
+		array(
+			'Quixplorer' => 'list',
+		),
+		array(
+			'access' => 'user,group',
+			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
+			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_quixplorer.xlf',
+		)
+	);
 }
 
-
-
-
-
-?>
+/*if (TYPO3_MODE=='BE')	{
+	t3lib_extMgm::addModule('tools','txt3quixplorerM1',"",t3lib_extMgm::extPath($_EXTKEY).'mod1/');
+}*/
